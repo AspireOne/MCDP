e1c88aa16c67cade46baccbbeab72530c57a31f2
Matěj Pešl <matejpesl1@gmail.com>
2020-02-05 22:12:36 +0100

    again added idea files

--- src/com/gmail/matejpesl1/mcdp/tools/Constants.java
+++ src/com/gmail/matejpesl1/mcdp/tools/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public enum Mode {DEBUG, BASICLAND}
 
 	public static final Mode mode = Mode.DEBUG;
-	public static final float PROGRAM_VERSION = 4.0f;
+	public static final float PROGRAM_VERSION = 4.1f;
 
 public static final String PATH_ROOT = System.getProperty("user.home");
 public static final File OWN_DIR = new File(PATH_ROOT +  "\\vysledky");
